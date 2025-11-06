a7ab9ad86249bd8a37b4ed2fe91a92f6bb197aad
unknown <chrisagrant10@gmail.com>
2025-11-06 12:47:45 +0700

    upload full project

---
+++ database/migrations/2025_10_21_165944_create_stocks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bahan_baku_id');
+            $table->bigInteger('quantity')->default(0);
+            $table->decimal('unit_price', 8, 2);
+            $table->foreignId('supplier_id');
+            $table->foreignId('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
